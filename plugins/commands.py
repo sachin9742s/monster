9a25a56047076f4d44f866fc1419b5f994d07e98
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-08-03 22:27:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,7 +68,6 @@ async def start(bot, cmd):
                     [
                         InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
                         InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
-                        InlineKeyboardButton('Support', url='https://t.me/KicchaRequest')
                     ]
                     ]
                 await bot.send_cached_media(
