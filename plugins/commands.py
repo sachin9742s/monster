bec440a5aeea746ddee621fcdd43708486aa56bd
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-08-04 10:45:47 +0530

    Create commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Other Bots", url="https://t.me/KicchaRequest")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
