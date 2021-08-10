d02dbda9dfe6405c2848332430d0c8d6612274e9
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-08-10 10:10:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -102,14 +102,11 @@ async def start(bot, cmd):
                         InlineKeyboardButton("🔎 Search Movies 🔍", switch_inline_query_current_chat=''),
                         InlineKeyboardButton("Request Group 🎟️", url="https://t.me/KicchaRequest")
                     ],
-                       InlineKeyboardButton("Group Owner 🧑‍💻", url="https://t.me/sachin_official_admin"),
-                       InlineKeyboardButton("About", callback_data="about")
-                    ],
+                       InlineKeyboardButton("About", callback_data="about"
                     [
-                        InlineKeyboardButton("Update Channel 🎫", url="https://t.me/KicchaRequest")
                     ]
                 ]
-            )
+            ))
         )
 
 
