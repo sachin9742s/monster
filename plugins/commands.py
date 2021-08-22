75b8814bfdd324a0d6a1be513a142086b7147fd5
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-08-22 10:24:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -198,4 +198,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
         ]
         ]
-    await query.message.edit(text="<b>Developer : <a href='https://t.me/sachin_official_admin'>Sachin_S</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href=''>🔒</a>\nUpdate Channel : <a href='https://t.me/GD_FILMCLUB'>GD_FILMCLUB</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>○ Creator : <a href='https://t.me/sachin_official_admin'>Sachin S</a>\n○ Language :  <code>Kannada</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nRequest Group : <a href='https://t.me/KicchaRequest'>Kiccha Request</a>\nUpdate Channel : <a href='https://t.me/GD_FILMCLUB'>GD_FILMCLUB</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True) 
