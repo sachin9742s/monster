6051883fef9b30dd33631289425636ab4e5d33f3
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-08-30 12:10:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,8 +100,7 @@ async def start(bot, cmd):
                 [
                     
                     [
-                        InlineKeyboardButton(
-                           "🎙  Add Me To Your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                        InlineKeyboardButton("🎙  Add Me To Your Group ➕", url="https://t.me/{BOT_USERNAME}?startgroup=true"),
                     ],
                     [
                         InlineKeyboardButton("🗣️Group", url="https://telegram.dog/KicchaRequest"),
