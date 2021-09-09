9cb7521e3eb2fac798cb5fda87a6eedf93551ba4
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-09 08:22:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,6 +82,7 @@ async def filter(client, message):
                 poster=await get_poster(search)
             if poster:
                 await message.reply_photo(photo=poster, caption=f"<b>🎬𝐅𝐢𝐥𝐦/𝐒𝐞𝐫𝐢𝐞𝐬 ☛ {search}\n🕐 𝐓𝐢𝐦𝐞 : <b>#Duration</b>\n📥 𝐌𝐨𝐯𝐢𝐞 𝐑𝐚𝐭𝐢𝐧𝐠 :<b>#Rating</b>\n📩 𝐈𝐦𝐝𝐛 : <a href="#IMDB_URL">𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a> \n \n© By:- [Kiccha Request](https://t.me/KicchaRequest) ‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+
             else:
                 await message.reply_text(f"<b>🎬𝙔𝙤𝙪𝙧 𝙈𝙤𝙫𝙞𝙚 ☛ {search} ‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
